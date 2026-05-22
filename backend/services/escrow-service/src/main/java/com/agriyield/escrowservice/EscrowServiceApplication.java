11fcbe754f35fd9861483f01be7cbacf089eda98
selamawitsh <selamshimeles9@gmail.com>
2026-05-22 21:12:04 +0300

    initialize escrow service module

---
+++ backend/services/escrow-service/src/main/java/com/agriyield/escrowservice/EscrowServiceApplication.java
@@ -0,0 +1,12 @@
+package com.agriyield.escrowservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscrowServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EscrowServiceApplication.class, args);
+    }
+}
